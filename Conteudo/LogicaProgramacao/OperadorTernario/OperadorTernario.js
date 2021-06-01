fcb74305b5e3939da27f25a53dcb0ebfc421f5a9
Gabriel-Alexandre <biellalex86@gmail.com>
2021-06-01 15:54:03 -0300

    operadorTernario

---
+++ Conteudo/LogicaProgramacao/OperadorTernario/OperadorTernario.js
@@ -0,0 +1,11 @@
+const pontuacao = 1001;
+const nivelUsuario = pontuacao >= 1000 ? 'Usuario vip' : 'Usuario não vip';
+// (condicao) ? (Valor true) : (Valor false);
+
+console.log(nivelUsuario);
+
+// if (pontuacao >= 1000) {
+//     console.log('Usuario vip');
+// }else {
+//     console.log('Usuario não vip');
+// }
