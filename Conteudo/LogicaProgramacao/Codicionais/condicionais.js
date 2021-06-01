c537054666908447cbbf4df51e12d9afa7b631dc
Gabriel-Alexandre <biellalex86@gmail.com>
2021-06-01 15:42:35 -0300

    condicionais + modelo html

---
+++ Conteudo/LogicaProgramacao/Codicionais/condicionais.js
@@ -0,0 +1,11 @@
+const nome = 'Gabriel Alexandre'
+
+if (false) {
+  console.log(nome + ' é lindo')
+}else if (false) {
+  console.log(nome + ' é bonito 1')
+}else if (false) {
+  console.log(nome + ' é bonito 2')
+}else {
+  console.log(nome + ' é bonito 3')
+}
